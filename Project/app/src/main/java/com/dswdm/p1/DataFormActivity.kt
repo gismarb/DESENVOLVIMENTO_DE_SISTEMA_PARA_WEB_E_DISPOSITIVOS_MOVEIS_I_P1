2c9d5e4da96dd439ed878a634797420c3e4da643
gismarb <gismar_barbosa@yahoo.com.br>
2020-10-19 16:11:28 -0300

    Iniciando construção da DataFormActivity

---
+++ Project/app/src/main/java/com/dswdm/p1/DataFormActivity.kt
@@ -0,0 +1,11 @@
+package com.dswdm.p1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dataform)
+    }
+}
