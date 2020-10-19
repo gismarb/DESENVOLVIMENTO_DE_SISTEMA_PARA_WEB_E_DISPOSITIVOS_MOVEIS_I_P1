521e6cf87bad385b4c3a71716ce1536a341505f4
gismarb <gismar_barbosa@yahoo.com.br>
2020-10-19 20:54:58 -0300

    Validação de Login OK, trabalhando na msg de ERRO

--- Project/app/src/main/java/com/dswdm/p1/MainActivity.kt
+++ Project/app/src/main/java/com/dswdm/p1/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.dswdm.p1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 
 
 // Mensagem de falha de Login
@@ -11,7 +14,11 @@ const val LOGIN_FAILURE_MESSAGE = "Atenção - e-mail/CPF e/ou senha inválidos"
 //const val USER_LOGIN_DD
 
 // Instanciando a Objetos para guardar usuários cadastrados "injetados"
-val userData = User("Gismar Barbosa", "111111111-22", "gismar@gismar.com", "g1sma@r")
+val user1 = User("Gismar Barbosa", "111111111-22", "gismar@gismar.com", "g1sm@r" )
+//val user2 = User("Ricardo Severiano Motta", "111111110-20", "ricardo@best.com.br", "r1c@rd0" )
+//val user3 = User("Marta Helena Richaauskka", "81111111-99", "mhrichaauskka@idle.com", "m@rt@H" )
+// Avaliar se irá usar uma lista
+//val users = listOf<User>(user1, user2, user3)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    //TODO: Modificar o instanciamento da variáveis para receber dados da classe criada (User).
+    //TODO: Modificar o instanciamento da variáveis para receber dados da classe criada (User). OK
 
     // Função para validar Login
     fun validateAccess(view: View) {
@@ -29,5 +36,14 @@ class MainActivity : AppCompatActivity() {
         val editTextPwd = findViewById<EditText>(R.id.editTextUserPwd)
         val userPwd = editTextPwd.text.toString()
 
+        if (userPwd.equals(user1.pwd) and userId.equals(user1.email) or userId.equals(user1.CPF)) {
+            val intent = Intent(this, DataFormActivity::class.java)
+            startActivity(intent)
+        } else {
+//            TODO: Repassar dados para uma TextView (neste caso, mensagem de erro para TextView)
+//            findViewById<TextView>(R.id.textViewErrorLogin) = LOGIN_FAILURE_MESSAGE
+        }
+
     }
+
 }
